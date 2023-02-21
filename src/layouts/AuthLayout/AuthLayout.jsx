131111a3c160dc6dab88e130608d4d9aeee0d445
Cristi Crudu <cristicrudu2002@gmail.com>
2023-02-21 23:11:44 +0200

    did it

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./AuthLayout.module.scss";
 import { useTranslation } from "react-i18next";
+import Bar from "@components/Navbar/Bar";
 
 export default function AuthLayout() {
     const { t } = useTranslation();
     return (
-        <main className={styles["container"]}>
-            <div className={styles["container__header"]}>
-                <div className={styles["container__cover"]}></div>
-                <h1 className={styles["container__title"]}>
-                    {t("auth-title")}
-                </h1>
-            </div>
-            <div className={styles["container__form"]}>
-                <Outlet />
-            </div>
-        </main>
+        <>
+            <Bar />
+            <main className={styles["container"]}>
+                <div className={styles["container__header"]}>
+                    <div className={styles["container__cover"]}></div>
+                    <h1 className={styles["container__title"]}>
+                        {t("auth-title")}
+                    </h1>
+                </div>
+                <div className={styles["container__form"]}>
+                    <Outlet />
+                </div>
+            </main>
+        </>
     );
 }
