36ebc1c7f01a5898f0efe4032401f65ae69a5a11
Andreea2506 <cornescu.andreea25@gmail.com>
2023-02-22 10:55:11 +0200

    added NewsJigsaw in Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,5 @@
 import ImageSlider from "../../components/ImageSlider/ImageSlider";
+import NewsJigsaw from "../../components/NewsJigsaw/NewsJigsaw"
 import Info_container from "../../components/Info_container/Info_container";
 import styles from "./Homepage.module.scss"
 
@@ -6,7 +7,7 @@ function Homepage() {
     return (
         <div className={styles['container']}>
             <div className={styles['container__slider']}><ImageSlider/></div>
-            <div className={styles['container__announces']}></div>
+            <div className={styles['container__announces']}><NewsJigsaw/></div>
             <div className={styles['container__information']}><Info_container/></div>
             <div className={styles['container__about']}></div>
         </div>
