9d1369ede01753681aac735d19b7267fae9991c1
Cristi Crudu <cristicrudu2002@gmail.com>
2023-03-05 18:26:55 +0200

    made context parent of browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route, useParams, Navigate } from "react-router-dom";
 import { useRouteLang } from "@hooks/useRouteLang";
-import { AuthContextProvider } from "@context/AuthContext";
 import AuthRoutes from "./routes/AuthRoutes";
 import { Suspense } from "react";
 import "./i18n";
 
 import GeneralLayout from "./layouts/GeneralLayout/GeneralLayout";
 import Homepage from "./pages/Homepage/Homepage";
-import NewsPage from "./pages/NewsPage";
 
 function App() {
     const { lang } = useParams();
@@ -15,14 +13,12 @@ function App() {
 
     return (
         <Suspense>
-            <AuthContextProvider>
-                <Routes>
-                    <Route path="/*" element={<AuthRoutes />} />
-                    <Route path="/" element={<GeneralLayout />}>
-                        <Route index exact element={<Homepage />} />
-                    </Route>
-                </Routes>
-            </AuthContextProvider>
+            <Routes>
+                <Route path="/*" element={<AuthRoutes />} />
+                <Route path="/" element={<GeneralLayout />}>
+                    <Route index exact element={<Homepage />} />
+                </Route>
+            </Routes>
         </Suspense>
     );
 }
