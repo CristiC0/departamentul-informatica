e7abc78a4eec63248c175c1eb238b0612f814f08
CristiC0 <82097873+CristiC0@users.noreply.github.com>
2023-03-08 10:42:59 +0200

    Auth/linked-backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthContextProvider } from "@context/AuthContext";
 import "./index.scss";
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -10,13 +11,15 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/ro" />} />
-                    <Route path="/:lang/*" element={<App />} />
-                </Routes>
-            </QueryClientProvider>
-        </BrowserRouter>
+        <AuthContextProvider>
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/ro" />} />
+                        <Route path="/:lang/*" element={<App />} />
+                    </Routes>
+                </QueryClientProvider>
+            </BrowserRouter>
+        </AuthContextProvider>
     </React.StrictMode>
 );
