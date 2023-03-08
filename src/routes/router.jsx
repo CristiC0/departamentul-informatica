f51b29685626391ffba71bdca50aa6e21817ddfa
Cristi Crudu <cristicrudu2002@gmail.com>
2023-03-08 22:32:48 +0200

    added teachers route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -10,6 +10,7 @@ import AuthLayout from "@layouts/AuthLayout/AuthLayout";
 import Homepage from "@pages/Homepage/Homepage";
 import LoginPage from "@pages/auth/Login/LoginPage";
 import RegisterPage from "@pages/auth/Register/RegisterPage";
+import Teachers from "@pages/teachers/Teachers";
 import Setup from "../Setup";
 
 const router = createBrowserRouter(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
             </Route>
             <Route path=":lang" element={<GeneralLayout />}>
                 <Route index exact element={<Homepage />} />
+                <Route path="teachers" element={<Teachers />} />
             </Route>
             <Route path="*" element={<Navigate to="/ro" />} />
         </Route>
