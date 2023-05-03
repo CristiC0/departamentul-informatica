23b0c2ce326092b03d74ba1a7d3bef454815b2cc
Andreea2506 <cornescu.andreea25@gmail.com>
2023-05-03 19:09:28 +0300

    editing the page with teachers

--- src/pages/teachers/Teachers.jsx
+++ src/pages/teachers/Teachers.jsx
@@ -8,17 +8,18 @@ import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import styles from "./Teachers.module.scss";
 import usePagination from "@hooks/usePagination";
+import CompartmentName from "@components/CompartmentName/CompartmentName"
 
-const selectMenuOptions = [
-    { title: "Profesori", value: "" },
-    { title: "Studenti", value: "" },
-    { title: "Cursuri", value: "" },
-];
+// const selectMenuOptions = [
+//     { title: "Profesori", value: "" },
+//     { title: "Studenti", value: "" },
+//     { title: "Cursuri", value: "" },
+// ];
 
 const defaultUserImage = <div className={styles["list__card--noPhoto"]}></div>;
 
 const teachers = [
-    { id: 1, name: "John Doe", photo: null },
+    { id: 1, name: "John Doe", function: "conferențiar universitar", photo: null },
     { id: 2, name: "John Doe", photo: null },
     { id: 3, name: "John Doe", photo: null },
     { id: 4, name: "John Doe", photo: null },
@@ -83,8 +84,9 @@ const Teachers = () => {
     } = usePagination(teachers, 6);
 
     return (
-        <>
-            <div className={styles.header}>
+        <div className={styles.container}>
+            <div className={styles["container__content"]}>
+                {/* <div className={styles.header}>
                 <div className={styles["header__select"]}>
                     <TabSelector
                         selectOptions={selectMenuOptions}
@@ -99,49 +101,51 @@ const Teachers = () => {
                     />
                     <VscSettings className={styles["search__filter"]} />
                 </div>
+            </div> */}
+                <CompartmentName name="Conducere"></CompartmentName>
+                <CompartmentName name="Cadre didactice"></CompartmentName>
+                <div className={styles.list}>
+                    {itemsOnPage.map((teacher) => (
+                        <div key={teacher.id} className={styles["list__card"]}>
+                            {teacher.photo ? (
+                                <img
+                                    src={teacher.photo}
+                                    alt={`${teacher.name} photo`}
+                                />
+                            ) : (
+                                defaultUserImage
+                            )}
+                            <h3><Link to={`${teacher.name}`}>{teacher.name}</Link></h3>
+                            <h4>
+                                {teacher.function}
+                            </h4>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.navigation}>
+                    <BsFillArrowLeftCircleFill
+                        className={styles["navigation__arrow"]}
+                        onClick={previousPage}
+                    />
+                    {displayedPages.map((page) => (
+                        <div
+                            className={`${styles["navigation__page"]} ${currentPage === page
+                                    ? styles["navigation__page--current"]
+                                    : ""
+                                }`}
+                            key={page}
+                            onClick={() => changePageNumber(page)}
+                        >
+                            {page}
+                        </div>
+                    ))}
+                    <BsFillArrowRightCircleFill
+                        className={styles["navigation__arrow"]}
+                        onClick={nextPage}
+                    />
+                </div>
             </div>
-            <div className={styles.list}>
-                {itemsOnPage.map((teacher) => (
-                    <div key={teacher.id} className={styles["list__card"]}>
-                        {teacher.photo ? (
-                            <img
-                                src={teacher.photo}
-                                alt={`${teacher.name} photo`}
-                            />
-                        ) : (
-                            defaultUserImage
-                        )}
-                        <h3>{teacher.name}</h3>
-                        <h4>
-                            <Link to={`${teacher.name}`}>Profile</Link>
-                        </h4>
-                    </div>
-                ))}
-            </div>
-            <div className={styles.navigation}>
-                <BsFillArrowLeftCircleFill
-                    className={styles["navigation__arrow"]}
-                    onClick={previousPage}
-                />
-                {displayedPages.map((page) => (
-                    <div
-                        className={`${styles["navigation__page"]} ${
-                            currentPage === page
-                                ? styles["navigation__page--current"]
-                                : ""
-                        }`}
-                        key={page}
-                        onClick={() => changePageNumber(page)}
-                    >
-                        {page}
-                    </div>
-                ))}
-                <BsFillArrowRightCircleFill
-                    className={styles["navigation__arrow"]}
-                    onClick={nextPage}
-                />
-            </div>
-        </>
+        </div>
     );
 };
 
