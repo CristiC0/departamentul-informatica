bd7a5455e5b5ba5de09b6c6d596fa29627ac6904
Cristi Crudu <cristicrudu2002@gmail.com>
2023-05-23 15:22:47 +0300

    changed hook to work when list is null

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -3,64 +3,75 @@ import { useState, useEffect } from "react";
 const usePagination = (list, nrOfItemsOnPage) => {
     const [pages, setPages] = useState({ displayedPages: [], currentPage: 1 });
     const [itemsOnPage, setItemsOnPage] = useState([]);
-
-    if (!list.length) return null;
-
-    const nrOfPages = Math.ceil(list.length / nrOfItemsOnPage);
-
-    const changePageNumber = (newPageNumber) => {
-        setPages((pages) => ({ ...pages, currentPage: newPageNumber }));
-    };
-
-    const nextPage = () => {
-        if (pages.currentPage < nrOfPages)
-            changePageNumber(pages.currentPage + 1);
-    };
-
-    const previousPage = () => {
-        if (pages.currentPage > 1) changePageNumber(pages.currentPage - 1);
-    };
+    const [info, setInfo] = useState({
+        nrOfPages: null,
+        changePageNumber: null,
+        nextPage: null,
+        previousPage: null,
+    });
 
     useEffect(() => {
-        const startPage =
-            nrOfPages < 5 || pages.currentPage <= 3
-                ? 1
-                : pages.currentPage >= nrOfPages - 2
-                ? nrOfPages - 4
-                : pages.currentPage - 2;
+        const nrOfPages =
+            list !== null ? Math.ceil(list.length / nrOfItemsOnPage) : null;
 
-        const endPage =
-            nrOfPages < 5 || pages.currentPage + 2 > nrOfPages
-                ? nrOfPages
-                : pages.currentPage <= 3
-                ? 5
-                : pages.currentPage + 2;
+        const changePageNumber = (newPageNumber) => {
+            setPages((pages) => ({ ...pages, currentPage: newPageNumber }));
+        };
 
-        const displayPages = [];
-        for (let i = startPage; i <= endPage; i++) displayPages.push(i);
+        const nextPage = () => {
+            if (pages.currentPage < nrOfPages)
+                changePageNumber(pages.currentPage + 1);
+        };
 
-        setPages((pages) => ({ ...pages, displayedPages: displayPages }));
+        const previousPage = () => {
+            if (pages.currentPage > 1) changePageNumber(pages.currentPage - 1);
+        };
 
-        const startItemIndex =
-            pages.currentPage === 1
-                ? 0
-                : (pages.currentPage - 1) * nrOfItemsOnPage;
-        const endItemIndex =
-            pages.currentPage * nrOfItemsOnPage - 1 < list.length
-                ? pages.currentPage * nrOfItemsOnPage - 1
-                : list.length;
+        setInfo({ nrOfPages, changePageNumber, nextPage, previousPage });
+    }, [list]);
 
-        setItemsOnPage(list.slice(startItemIndex, endItemIndex + 1));
-    }, [pages.currentPage]);
+    useEffect(() => {
+        if (list && list.length) {
+            const startPage =
+                info.nrOfPages < 5 || pages.currentPage <= 3
+                    ? 1
+                    : pages.currentPage >= info.nrOfPages - 2
+                    ? info.nrOfPages - 4
+                    : pages.currentPage - 2;
+
+            const endPage =
+                info.nrOfPages < 5 || pages.currentPage + 2 > info.nrOfPages
+                    ? info.nrOfPages
+                    : pages.currentPage <= 3
+                    ? 5
+                    : pages.currentPage + 2;
+
+            const displayPages = [];
+            for (let i = startPage; i <= endPage; i++) displayPages.push(i);
+
+            setPages((pages) => ({ ...pages, displayedPages: displayPages }));
+
+            const startItemIndex =
+                pages.currentPage === 1
+                    ? 0
+                    : (pages.currentPage - 1) * nrOfItemsOnPage;
+            const endItemIndex =
+                pages.currentPage * nrOfItemsOnPage - 1 < list.length
+                    ? pages.currentPage * nrOfItemsOnPage - 1
+                    : list.length;
+
+            setItemsOnPage(list.slice(startItemIndex, endItemIndex + 1));
+        }
+    }, [pages.currentPage, list]);
 
     return {
-        nrOfPages,
+        nrOfPages: info.nrOfPages,
         displayedPages: pages.displayedPages,
         itemsOnPage,
-        changePageNumber,
+        changePageNumber: info.changePageNumber,
         currentPage: pages.currentPage,
-        nextPage,
-        previousPage,
+        nextPage: info.nextPage,
+        previousPage: info.previousPage,
     };
 };
 
