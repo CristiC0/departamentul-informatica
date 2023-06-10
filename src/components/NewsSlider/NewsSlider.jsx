07d2d69bda12f378af3d9c42aa56106125fd5553
Cristi Crudu <cristicrudu2002@gmail.com>
2023-06-10 15:39:36 +0300

    updated newsSlider

--- src/components/NewsSlider/NewsSlider.jsx
+++ src/components/NewsSlider/NewsSlider.jsx
@@ -1,21 +1,23 @@
-import styles from './NewsSlider.module.scss'
-import { useRef, useState, useEffect } from 'react';
-import { register } from 'swiper/element/bundle';
+import styles from "./NewsSlider.module.scss";
+import { useRef, useState, useEffect } from "react";
+import { register } from "swiper/element/bundle";
 import { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
-import axios from 'axios';
+import axios from "axios";
 import useNews from "@hooks/useNews";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 register();
 export default function NewsSlider() {
-
     const swiperElRef = useRef(null);
     const [data, setData] = useState(null);
+    const { i18n } = useTranslation();
 
     const swiperParams = {
-        modules: { Navigation, Pagination, Autoplay, EffectFade},
+        modules: { Navigation, Pagination, Autoplay, EffectFade },
         slidesPerView: 3,
         spaceBetween: 20,
-        navigation:true,
+        navigation: true,
         // breakpoints: {
         //     992: {
         //         slidesPerView: 3,
@@ -33,7 +35,7 @@ export default function NewsSlider() {
         },
         effect: "fade",
         injectStyles: [
-          `
+            `
             .swiper-button-next,
             .swiper-button-prev {
               color: white;
@@ -54,22 +56,21 @@ export default function NewsSlider() {
             Object.assign(swiperElRef.current, swiperParams);
             swiperElRef.current.initialize();
         }
-    }, [swiperElRef,swiperParams]);
-    
+    }, [swiperElRef, swiperParams]);
+
     useEffect(() => {
         axios
             .get(`${import.meta.env.VITE_API_BASE_URL}/news`)
             .then((response) => {
                 setData(response.data);
-            })
+            });
     }, []);
 
-    if (data === null) return (<>Loading...</>)
-    const priorityNews = data.filter((data) => (data.priority == 1));
+    if (data === null) return <>Loading...</>;
+    const priorityNews = data.filter((data) => data.priority == 1);
 
     return (
         <div className={styles.container}>
-
             <swiper-container
                 ref={swiperElRef}
                 init="true"
@@ -78,17 +79,38 @@ export default function NewsSlider() {
                 {priorityNews.map((news) => (
                     <swiper-slide key={news.id} className={styles.swiper}>
                         <div className={`card ${styles.card}`}>
-                            <img src={news.thumbnail} className="card-img-top" alt="..." />
+                            <img
+                                src={news.thumbnail}
+                                className="card-img-top"
+                                alt="..."
+                            />
                             <div className={`card-body ${styles.card__body}`}>
-                                <h5 className={`card-title ${styles.card__title}`}>{news.title}</h5>
-                                <p className={`card-text ${styles.card__text}`}>{news.createdAt}</p>
+                                <h5
+                                    className={`card-title ${styles.card__title}`}
+                                >
+                                    {news.title}
+                                </h5>
+                                <p className={`card-text ${styles.card__text}`}>
+                                    {new Date(
+                                        news.createdAt
+                                    ).toLocaleDateString("en-GB")}
+                                </p>
                             </div>
                             <div className={styles.overlay}>
-                                <h5 className={styles.overlay__name}>{news.title}</h5>
+                                <h5 className={styles.overlay__name}>
+                                    {news.title}
+                                </h5>
                                 <div className={styles.overlay__text}>
-                                    <p className={styles.overlay__description}>{news.content}</p>
+                                    <p className={styles.overlay__description}>
+                                        {news.content?.[0]?.content?.[0]?.text}
+                                    </p>
                                 </div>
-                                <a href="#" className={`btn btn-primary ${styles.overlay__button}`}>Acceseaza cursul</a>
+                                <Link
+                                    to={`/${i18n.language}/news/${news.id}`}
+                                    className={`btn btn-primary ${styles.overlay__button}`}
+                                >
+                                    Acceseaza
+                                </Link>
                             </div>
                         </div>
                     </swiper-slide>
